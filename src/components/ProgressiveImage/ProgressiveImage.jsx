5f7b2140f46ca0cb1919ad711a9e04cbb234da85
Jon Varner <jon.varner@gmail.com>
2019-01-20 13:15:39 -0800

    Added progressive image loading component

---
+++ src/components/ProgressiveImage/ProgressiveImage.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class ProgressiveImage extends Component {
+  state = {
+    image: this.props.preview,
+    loading: true,
+  };
+
+  componentDidMount() {
+    console.log('image loading');
+    this.loadImage(this.props.src);
+  }
+
+  loadImage = src => {
+    const img = new Image();
+    img.src = src;
+    img.onload = () => {
+      console.log('image loaded');
+      this.setState({
+        image: img.src,
+        loading: false,
+      });
+    };
+  };
+
+  style = loading => {
+    return {
+      transition: '0.5s filter linear',
+      filter: `${loading ? 'blur(50px' : ''}`,
+    };
+  };
+
+  render() {
+    return (
+      <img
+        style={this.style(this.state.loading)}
+        src={this.state.image}
+        alt={this.props.alt}
+      />
+    );
+  }
+}
+
+ProgressiveImage.propTypes = {
+  preview: PropTypes.string.isRequired,
+  src: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired,
+};
+
+export default ProgressiveImage;
